6d742543ab7183376d645f9155dbb094d12c339c
Romain SIMON <contact@romainsimon.net>
2018-12-09 14:09:59 +0100

    Fix: large genome example

--- examples/largeGenome.js
+++ examples/largeGenome.js
@@ -2,10 +2,11 @@
 
 const { Population } = require('../index')
 
-// const fitnessFunction = () => Math.random()
+// This fitness function will make genomes grow larger and larger
 const fitnessFunction = genome => genome.connections.length
 
 const population = new Population(20, 3, 3)
+
 for (let i = 1; i < 10; ++i) {
   population.evolve(10, fitnessFunction)
 }
